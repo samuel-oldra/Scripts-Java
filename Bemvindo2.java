10d4e3604f2240cd5393509ac3fd9124d4e40d9d
Samuel B. Oldra <samuel.oldra@gmail.com>
2003-06-04 19:00:00 -0300

    Bem-vindo 2

---
+++ Bemvindo2.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Bemvindo2{
+	// método que inicia a execução de controle
+	public static void main(String args[]){
+		JOptionPane.showMessageDialog(null,"Bem vindo a programação 3");
+		// exibe uma mensagem numa caixa de dialogo no centro da tela (null)
+		System.exit(0);
+		// termina o aplicativo
+	}
+}
