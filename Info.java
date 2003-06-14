9b41bfdf644c30e46645360495a4168ffceba4b5
Samuel B. Oldra <samuel.oldra@gmail.com>
2003-06-14 19:00:00 -0300

    Info

---
+++ Info.java
@@ -0,0 +1,29 @@
+/*  ::: NOTAÇÃO USADA :::
+ *
+ * i    = variáveis int
+ * f  	= variáveis float
+ * b  	= variáveis boolean
+ * c  	= variáveis char
+ * s  	= variáveis string
+ * p	= variáveis ponteiro
+ * e	= variáveis estrutura
+ * ins	= instância da classe
+ * Caps	= classes
+ *
+ */
+ 
+ /*  ::: DADOS DO PROGRAMADOR :::
+ * Aluno: Samuel Brando Oldra
+ * Matrícula: 10205981
+ * Disciplina: Programação 3
+ * Professor: Alexandre Zamerlam
+ * Faculdade: Faculdade de Informática de Taquara
+ * Mantenedora: Fundação Educacional Encosta Inferior do Nordeste
+ */
+ 
+ public class Info{
+ 	// váriáveis privadas
+ 	// variáveis públicas
+ 	
+ 	// métodos de acesso para variáveis privadas
+ }
