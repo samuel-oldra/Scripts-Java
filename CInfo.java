e092d18f25a23caed99feced7b215a19a2d8b34c
Samuel B. Oldra <samuel.oldra@gmail.com>
2004-06-07 00:00:00 -0300

    Info

---
+++ CInfo.java
@@ -0,0 +1,19 @@
+/*	::: NOTAÇÃO USADA :::
+ *
+ * i  = variáveis int
+ * f  = variáveis float
+ * b  = variáveis boolean
+ * c  = variáveis char
+ * s  = variáveis string
+ * in = instância da classe
+ * md = métodos
+ * C  = classes
+ *
+ */
+ 
+ public class CInfo{
+ 	// váriáveis privadas
+ 	// variáveis públicas
+ 	
+ 	// métodos de acesso
+ }
